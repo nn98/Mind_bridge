d03aa4727773a69270d21a737cf046fbf8c19726
Andrew Kim <38521355+nn98@users.noreply.github.com>
2025-07-04 11:54:14 +0900

    Feat(API) : Update API name for local test

--- src/App.js
+++ src/App.js
@@ -34,6 +34,8 @@ import { buttonLabels } from './constants/buttonLabels';
 import { formLinks } from './constants/formLinks';
 
 const App = () => {
+  console.log(process.env.REACT_APP_KEY);
+  console.log(process.env.REACT_APP_MAP_KEY);
   const [selectedBoard, setSelectedBoard] = useState('');
   const [selectedChat, setSelectedChat] = useState(null);
   const [isAdmin] = useState(false);
