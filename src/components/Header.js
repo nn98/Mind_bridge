32c7ee67786d020b2af9bb835bb262d60bafec2c
leejaejun4 <jaejun1623@gmail.com>
2025-07-31 18:21:56 +0900

    ;;

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { Link, useNavigate, useLocation } from "react-router-dom";
 import { sectionLabels } from "../constants/sectionLabels";
-import { SignedIn, SignedOut, UserButton, useUser } from "@clerk/clerk-react";
+import { UserButton, useUser } from "@clerk/clerk-react";
 
 const Header = ({ introRef, servicesRef, infoRef, setScrollTarget }) => {
   const navigate = useNavigate();
