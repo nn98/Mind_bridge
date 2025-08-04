8e975fa3375787d79a3f6e60064c2c62c3bfdb77
KIMJUNGYEON-MIHO <kimmiho7369@naver.com>
2025-08-04 11:40:00 +0900

    pom.xml 중복부분 삭제,postController 삭제부분 오류수정,JwtAuthenticationFilter 에 @nonnull 추가  - 김정연-

--- src/main/java/com/example/backend/security/JwtAuthenticationFilter.java
+++ src/main/java/com/example/backend/security/JwtAuthenticationFilter.java
@@ -1,13 +1,7 @@
 package com.example.backend.security;
 
-import com.auth0.jwt.interfaces.DecodedJWT;
-import jakarta.servlet.FilterChain;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+import java.io.IOException;
+import java.util.List;
 
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -15,10 +9,16 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
-import java.util.List;
 
-import java.io.IOException;
-import java.util.Collections;
+import com.auth0.jwt.interfaces.DecodedJWT;
+
+import jakarta.annotation.Nonnull;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Component
@@ -29,7 +29,10 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     private final CustomUserDetailsService userDetailsService;
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+    protected void doFilterInternal(
+        @Nonnull HttpServletRequest request, 
+        @Nonnull HttpServletResponse response, 
+        @Nonnull FilterChain filterChain)
             throws ServletException, IOException {
 
         String authHeader = request.getHeader("Authorization");
