0c00210538540f43b99affd381e90ddf37a5366a
NaSeJong1 <135085560+NaSeJong1@users.noreply.github.com>
2025-08-07 10:17:59 +0900

    Feat: counselling entity add

---
+++ src/main/java/com/example/backend/entity/Counselling.java
@@ -0,0 +1,30 @@
+package com.example.backend.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "counselling")
+@Getter @Setter @NoArgsConstructor
+public class Counselling {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "counsel_id")
+    private Long counselId;
+
+    // email은 다른 테이블과 연결되는 외래키로 가정
+    @Column(nullable = false)
+    private String email;
+
+    @Column(name = "user_counselling_summation")
+    private String userCounsellingSummation;
+
+    @Column(name = "user_counselling_emotion")
+    private String userCounsellingEmotion;
+
+    @Column(name = "counselor_summation")
+    private String counselorSummation;
+}
