615040d28a16141df4143154cf40932f85509a81
kdh20137 <kdh2013743@gmail.com>
2025-08-12 10:36:13 +0900

    Design: 공통 input css 제거 후 수정

--- src/components/about/AboutSection/ContactForm.js
+++ src/components/about/AboutSection/ContactForm.js
@@ -15,10 +15,10 @@ const ContactForm = () => (
             }
         }}>
         <div className="row-two">
-            <div className="form-group"><label htmlFor="name">이름</label><input id="name" type="text" name="name" required /></div>
+            <div className="form-group"><label htmlFor="name">이름</label><input id="name" type="name" name="name" required /></div>
             <div className="form-group"><label htmlFor="email">이메일</label><input id="email" type="email" name="email" required /></div>
         </div>
-        <div className="form-group"><label htmlFor="title">제목</label><input id="title" type="text" name="title" required /></div>
+        <div className="form-group"><label htmlFor="title">제목</label><input id="title" type="title" name="title" required /></div>
         <div className="form-group"><label htmlFor="message">메시지</label><textarea id="message" name="message" rows={5} required /></div>
         <button type="submit">보내기</button>
     </form>
