bcd465a9e87ce8bcec0bcc4a778ee7f85a52b5e7
KIMJUNGYEON-MIHO <kimmiho7369@naver.com>
2025-08-22 13:02:49 +0900

    Refactor : 게시판 조회

--- src/components/board/services/apiClient.js
+++ src/components/board/services/apiClient.js
@@ -8,11 +8,5 @@ const apiClient = axios.create({
     withCredentials: true,
 });
 
-// 모든 요청에 토큰 자동 주입
-apiClient.interceptors.request.use((config) => {
-    const token = localStorage.getItem("token");
-    if (token) config.headers.Authorization = `Bearer ${token}`;
-    return config;
-});
 
 export default apiClient;
