5216608d97369b9f4c22c3ab1bc3db78d79e3a25
Andrew Kim <38521355+nn98@users.noreply.github.com>
2025-09-03 15:58:14 +0900

    Feat(RESTful): 회원정보-비밀번호 변경 방식 수정

--- src/main/java/com/example/backend/dto/user/UpdateRequest.java
+++ src/main/java/com/example/backend/dto/user/UpdateRequest.java
@@ -3,11 +3,9 @@ package com.example.backend.dto.user;
 
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
-import lombok.Setter;
 import lombok.ToString;
 
 @Getter
-@Setter
 @ToString
 public class UpdateRequest {
     @Size(min = 2, max = 20, message = "닉네임은 2-20자 사이여야 합니다")
