887098b3aac263a65f5d1858c27df4fd5d5687b5
NaSeJong1 <135085560+NaSeJong1@users.noreply.github.com>
2025-09-08 18:04:48 +0900

    test:데이터 확인

--- src/main/java/com/example/backend/controller/UserController.java
+++ src/main/java/com/example/backend/controller/UserController.java
@@ -51,6 +51,7 @@ public class UserController {
 
     @GetMapping("/availability")
     public ResponseEntity<Map<String, Boolean>> checkAvailability(@RequestParam AvailabilityType type, @RequestParam String value) {
+        System.out.println("type = " + type);
         boolean isAvailable = switch (type) {
             case NICKNAME -> userService.isNicknameAvailable(value);
             case EMAIL    -> userService.isEmailAvailable(value);
